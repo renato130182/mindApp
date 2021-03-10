0213e64d12f134f9490f22c3efb03c2057f45b23
Renato Ferreira Soares <35581699+renato130182@users.noreply.github.com>
2021-03-10 20:52:57 -0300

    Finalizada tarefa CRUD,Adicionada deleção e refotado controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,6 @@
 class ContentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_content, only: [:show, :edit, :update, :destroy]
     def index
         @contents = current_user.contents
     end
@@ -10,16 +11,13 @@ class ContentsController < ApplicationController
 
     def create
         @content = current_user.contents.build(content_params)
-
         if @content.save
             redirect_to contents_path, notice: "Content successfully created!"
         else
             render :new
         end
     end
-    def edit        
-        @content = Content.find(params[:id])
-    end
+    def edit; end
 
     def update
         @content = Content.find(params[:id])
@@ -30,10 +28,20 @@ class ContentsController < ApplicationController
         end
     end
 
-    def show
-        @content = Content.find(params[:id])
+    def show; end
+
+    def destroy        
+        if @content.destroy
+            redirect_to contents_path, notice: "Content successfully destroyed!"
+        else
+            redirect_to contents_path, notice: "Content destroy failed!"
+        end
     end
     private 
+
+    def set_content
+        @content = Content.find(params[:id])
+    end
 
     def content_params
         params.require(:content).permit(:title, :description)
