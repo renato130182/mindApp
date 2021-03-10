473ff5f469362406d54d86734c1ce6ae674606f1
Renato Ferreira Soares <35581699+renato130182@users.noreply.github.com>
2021-03-10 20:20:31 -0300

    Adicionada tela de edição de contrnt

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,6 +5,25 @@ class ContentsController < ApplicationController
     end
 
     def new
-        @contents = Content.new
+        @content = Content.new
     end
-  end
+
+    def create
+        puts 'aqui'
+        @content = current_user.contents.build(content_params)
+
+        if @content.save
+            redirect_to contents_path, notice: "Content successfully created!"
+        else
+            render :new
+        end
+    end
+    def edit
+        @content = Content.find(params[:id])
+    end
+    private 
+
+    def content_params
+        params.require(:content).permit(:title, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :contents
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
